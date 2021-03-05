4d5966e9e54a1978abfa432fde8556754387e97a
zapatran <zapatran@users.noreply.github.com>
2021-03-05 20:15:24 +0100

    add game

--- src/index.js
+++ src/index.js
@@ -43,8 +43,13 @@ function destroySplashScreen() {
   destroyDom(splashScreen);
 }
 
+function destroyGameScreen() {
+  destroyDom(gameScreen);
+}
+
 function startGameClick() {
   destroySplashScreen();
+  buildGameScreen();
 }
 
 function buildSplashScreen() {
@@ -56,6 +61,83 @@ function buildSplashScreen() {
   splashScreen
     .querySelector('.button')
     .addEventListener('click', startGameClick);
+}
+
+function vikingAttackClicked() {
+  var message = war.vikingAttack();
+  renderSoldiers('saxon');
+  renderAttackMessage(message);
+}
+
+function saxonAttackClicked() {
+  var message = war.saxonAttack();
+  renderSoldiers('viking');
+  renderAttackMessage(message);
+}
+
+function renderAttackMessage(message) {
+  gameScreen.querySelector('.status-message').innerText = message;
+}
+
+function renderSoldiers(faction) {
+  var army;
+  var list;
+
+  switch (faction) {
+    case 'viking':
+      army = war.vikingArmy;
+      list = gameScreen.querySelector('#viking-army ul');
+      break;
+    case 'saxon':
+      army = war.saxonArmy;
+      list = gameScreen.querySelector('#saxon-army ul');
+      break;
+  }
+
+  list.innerHTML = '';
+  for (var i = 0; i < army.length; i++) {
+    var soldier = army[i];
+    var item = document.createElement('li');
+    item.classList.add('soldier');
+    item.innerHTML = `
+            <li class="soldier ${soldier.health <= 0 ? 'sodier--dead' : ''}">
+                <img src="images/${faction}.png" title="${
+      soldier.name
+    }. Health: ${soldier.health}. Strength: ${soldier.strength}." width="50">
+                <p>${soldier.health}</p>
+            </li>
+        `;
+    list.appendChild(item);
+  }
+}
+
+function buildGameScreen() {
+  gameScreen = buildDom(`
+        <div id="viking-army" class="army">
+            <h2>Vikings</h2>
+            <ul></ul>
+        </div>
+        <div id="saxon-army" class="army">
+            <h2>Saxons</h2>
+            <ul></ul>
+        </div>
+        <div class="status-message">
+            Click a button to start fighting!
+        </div>
+        <div class="attacks">
+            <button id="viking-attack" class="button">Viking Attack</button>
+            <button id="saxon-attack" class="button">Saxon Attack</button>
+        </div>  
+    `);
+
+  var vikingAttackButton = gameScreen.querySelector('#viking-attack');
+  var saxonAttackButton = gameScreen.querySelector('#saxon-attack');
+
+  vikingAttackButton.addEventListener('click', vikingAttackClicked);
+  saxonAttackButton.addEventListener('click', saxonAttackClicked);
+
+  renderSoldiers('viking');
+  renderSoldiers('saxon');
 }
 
 function loadGame() {
